402720ea97373e178ea35b59559200ffb0de53e2
Wilton <wiltonjunioralvess@gmail.com>
2026-05-20 13:18:03 -0400

    adicionei os metodos para enviar notificações para o email

--- src/main/java/com/wiltonjunior/notificacao/NotificacaoApplication.java
+++ src/main/java/com/wiltonjunior/notificacao/NotificacaoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificacaoApplication {
+	//rmsr yluw cxga xbtw
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificacaoApplication.class, args);
